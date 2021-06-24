3b021a130c005af67bf79927762b33f590088c5a
murilokrugner <murilokrugner@hotmail.com>
2021-06-24 13:31:16 -0300

    port

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-   // baseURL: 'https://knowledgesoftware.kinghost.net/apps_nodejs/droid/dist/',
-    baseURL: 'http://localhost:3333/',
+    baseURL: 'https://knowledgesoftware.kinghost.net/apps_nodejs/droid/dist/',
+    //baseURL: 'http://localhost:3333/',
 });
 
 export default api;
